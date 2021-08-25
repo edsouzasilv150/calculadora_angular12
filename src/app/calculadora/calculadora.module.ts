accbc276005a10836662666a1db2a43f3ff66a91
Edilson Sousa <edsouzasilv@hotmail.com>
2021-08-25 12:34:01 -0300

    Criando  o service e movendo os arquivos do componentes para o diretorio correto.

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CalculadoraComponent } from './componentes/calculadora';
+import { CalculadoraComponent } from './componentes';
 
 @NgModule({
   declarations: [
